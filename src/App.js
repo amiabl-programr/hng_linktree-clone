14fc2ecc4dccfed1e323be5124fa17774beb0e96
AyoOlu1 <oluwayemivictor15@gmail.com>
2022-11-01 07:33:35 +0100

    fix profile section

--- src/App.js
+++ src/App.js
@@ -15,21 +15,23 @@ function App() {
     <>
       <main>
 
-        <div className='profile__img-container'>
-          <div>
+        <div className='profile'>
+          <div className='profile__img-container'>
             <img src={profile_img} id="profile__img" alt="profile-image" />
             <span id='twitter'>vicayo_oluwa</span>
             <span id='slack'>vicayo</span>
           </div>
 
-          <div className="tooltip">
-            <img src={share_icon}  alt="" />
-            <span className="tooltiptext"> Share Link</span>
+          <div className='tooltip__container'>
+            <div className="tooltip">
+              <img src={share_icon} alt="" />
+              <span className="tooltiptext"> Share Link</span>
+            </div>
           </div>
         </div>
 
         <div className='grids'>
-          <a href="" rel='noopener noreferrer' id="" className='grid'>Twitter Link</a>
+          <a href="https://twitter.com/vicayo_oluwa" rel='noopener noreferrer' id="" className='grid'>Twitter Link</a>
           <a href="https://training.zuri.team/" rel='noopener noreferrer' id="btn__zuri" className='grid'>Zuri Team</a>
           <a href="http://books.zuri.team" title='find books about design and coding here' rel='noopener noreferrer' id="books" className='grid'>Zuri Books</a>
           <a href="https://books.zuri.team/python-for-beginners?ref_id=victoroluwayemi" rel='noopener noreferrer' id="book__python" className='grid'>Python Books</a>
