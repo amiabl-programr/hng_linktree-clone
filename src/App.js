2e03042646cf7dcf2d945044081d161cbf6067d5
AyoOlu1 <oluwayemivictor15@gmail.com>
2022-11-01 05:57:13 +0100

    add links

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import React from 'react';
 import profile_img from "./assets/profile-img.jpg";
 import Zuri_logo from "./assets/Zuri.Internship_Logo.svg";
 import I4G_Logo from "./assets/I4G.svg";
+import slack_icon from "./assets/slack-icon.svg";
+import github_icon from "./assets/github-icon.svg";
+
 import './App.css';
 
 function App() {
@@ -13,25 +16,36 @@ function App() {
 
         <div className='profile__img-container'>
           <img src={profile_img} id="profile__img" alt="profile-image" />
-          <span>Annette Black</span>
+          <span id='twitter'>vicayo_oluwa</span>
+          <span id='slack'>vicayo</span>
         </div>
 
         <div className='grids'>
-          <div className='grid'>Twitter Link</div>
-          <div className='grid'>Zuri Team</div>
-          <div className='grid'>Zuri Books</div>
-          <div className='grid'>Python Books</div>
-          <div className='grid'>Background Check for Coders</div>
-          <div className='grid'>Design Books</div>
+          <a href="" rel='noopener noreferrer' id="" className='grid'>Twitter Link</a>
+          <a href="https://training.zuri.team/" rel='noopener noreferrer' id="btn__zuri" className='grid'>Zuri Team</a>
+          <a href="http://books.zuri.team" title='find books about design and coding here' rel='noopener noreferrer' id="books" className='grid'>Zuri Books</a>
+          <a href="https://books.zuri.team/python-for-beginners?ref_id=victoroluwayemi" rel='noopener noreferrer' id="book__python" className='grid'>Python Books</a>
+          <a href="https://background.zuri.team" rel='noopener noreferrer' id="pitch" className='grid'>Background Check for Coders</a>
+          <a href="https://books.zuri.team/design-rules " rel='noopener noreferrer' id="book__design" className='grid'>Design Books</a>
+
+          <div className='icons'>
+            <span>
+              <img src={slack_icon} alt="slack_icon" />
+            </span>
+            <span>
+              <img src={github_icon} alt="github_icon" />
+            </span>
+          </div>
         </div>
+
+
         <footer>
           <div>
-            <img src={Zuri_logo} alt="" />
+            <img src={Zuri_logo} alt="Zuri_logo" />
           </div>
           <p>HNG Internship 9 Frontend Task</p>
           <div>
-            <img src={I4G_Logo} alt="" />
-            <img src="." alt="" />njk
+            <img src={I4G_Logo} alt="I4G_Logo" />
           </div>
         </footer>
       </main>
